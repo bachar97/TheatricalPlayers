755002fd90e8dc1b863cb342f565af48f14e6bcb
Bachar Sabra <bacharsabra@hotmail.com>
2022-11-02 19:23:19 +0100

    Added Customer class

---
+++ src/main/java/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    public String clientID;
+    public String customerName;
+    public int fidelityBalance;
+
+    public Customer(String clientID, String customerName, int fidelityBalance){
+        this.clientID = clientID;
+        this.customerName = customerName;
+        this.fidelityBalance = fidelityBalance;
+    }
+}
